47ee6eeaee071a7ed1089d8bbf67a12eca0230c0
Tatiana Dias <tati.m.d@hotmail.com>
2018-10-17 16:48:04 -0300

    teste

--- app/src/main/java/com/example/andreza/harvardmuseums/LoginActivity.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.gabriel.harvardmuseums;
+package com.example.andreza.harvardmuseums;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.andreza.harvardmuseums.R;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/example/andreza/harvardmuseums/SplashHarvardMuseusm.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/SplashHarvardMuseusm.java
@@ -28,24 +28,3 @@ package com.digitalhouse.digitalhouseapp;
         import java.util.Timer;
         import java.util.TimerTask;
 
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                irParaLogin();
-            }
-        }, 3000);
-
-    }
-
-    public void irParaLogin(){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-}
