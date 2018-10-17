993f7480083a352fdacc5a78952c6b99639cb86d
francorufino <campbell.andreza@gmail.com>
2018-10-17 14:28:18 -0300

    alteracao de cor

---
+++ app/src/main/java/com/example/andreza/harvardmuseums/SplashHarvardMuseusm.java
@@ -0,0 +1,51 @@
+package com.example.andreza.harvardmuseums;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashHarvardMuseusm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_harvard_museusm);
+
+        new Timer().;
+    }
+}
+
+
+package com.digitalhouse.digitalhouseapp;
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import java.util.Timer;
+        import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                irParaLogin();
+            }
+        }, 3000);
+
+    }
+
+    public void irParaLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
