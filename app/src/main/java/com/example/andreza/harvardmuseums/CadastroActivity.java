3273c3ea753e2261eea529051e6bd3bb97334e1c
RaphaelBathe <raphabathe@gmail.com>
2018-10-22 20:39:34 -0300

    atualizacao cadastro activity

---
+++ app/src/main/java/com/example/andreza/harvardmuseums/CadastroActivity.java
@@ -0,0 +1,34 @@
+package com.example.andreza.harvardmuseums;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CadastroActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+
+        Button register = findViewById(R.id.register_button_id);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HarvardMuseumsMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
+
+
+
--- app/src/main/java/com/example/andreza/harvardmuseums/LoginActivity.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/LoginActivity.java
@@ -38,9 +38,6 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity {
 
 
@@ -50,44 +47,90 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView register = findViewById(R.id.register_now_id);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button loginClicado = findViewById(R.id.login_button);
+        loginClicado.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HarvardMuseumsMainActivity.class);
+
+                Bundle bundle = new Bundle();
+
+                final AutoCompleteTextView emailDigitado = findViewById(R.id.email_id);
+                final EditText passwordDigitado = findViewById(R.id.password_id);
+
+                Button buttonLogin = findViewById(R.id.login_button);
+
+                if (emailDigitado.getText().toString().equals(passwordDigitado.getText().toString())) {
+                    bundle.putString(CHAVE_EMAIL, emailDigitado.getText().toString());
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                } else {
+                    emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                    passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                    Snackbar.make(buttonLogin, "Email e/ou senha incorreto(s)", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Ok, entendi.", new View.OnClickListener(){
+                                @Override
+                                public void onClick(View view) {
+                                    emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
+                                    passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
+                                }
+                            })
+                            .show();
+                }
+
+            }
+        });
     }
 
-    public void loginClicado(View view) {
-        Intent intent = new Intent(this, HarvardMuseumsMainActivity.class);
 
-        Bundle bundle = new Bundle();
 
-        final AutoCompleteTextView emailDigitado = findViewById(R.id.email_id);
-        final EditText passwordDigitado = findViewById(R.id.password_id);
 
-        Button buttonLogin = findViewById(R.id.login_button);
+//    public void loginClicado(View view) {
+//        Intent intent = new Intent(this, HarvardMuseumsMainActivity.class);
+//
+//        Bundle bundle = new Bundle();
+//
+//        final AutoCompleteTextView emailDigitado = findViewById(R.id.email_id);
+//        final EditText passwordDigitado = findViewById(R.id.password_id);
+//
+//        Button buttonLogin = findViewById(R.id.login_button);
+//
+//        if (emailDigitado.getText().toString().equals(passwordDigitado.getText().toString())) {
+//            bundle.putString(CHAVE_EMAIL, emailDigitado.getText().toString());
+//            intent.putExtras(bundle);
+//            startActivity(intent);
+//        } else {
+//            emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+//            passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+//
+//            Snackbar.make(buttonLogin, "Email e/ou senha incorreto(s)", Snackbar.LENGTH_INDEFINITE)
+//                    .setAction("Ok, entendi.", new View.OnClickListener(){
+//                        @Override
+//                        public void onClick(View view) {
+//                            emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
+//                            passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
+//                        }
+//                    })
+//                    .show();
+//        }
+//
+//    }
 
-        if (emailDigitado.getText().toString().equals(passwordDigitado.getText().toString())) {
-            bundle.putString(CHAVE_EMAIL, emailDigitado.getText().toString());
-            intent.putExtras(bundle);
-            startActivity(intent);
-        } else {
-            emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-            passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
 
-            Snackbar.make(buttonLogin, "Email e/ou senha incorreto(s)", Snackbar.LENGTH_INDEFINITE)
-                    .setAction("Ok, entendi.", new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view) {
-                            emailDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
-                            passwordDigitado.setTextColor(getResources().getColor(R.color.colorPrimary));
-                        }
-                    })
-                    .show();
-        }
-
+    public void registerNow(View view) {
+        Intent intent = new Intent(this,CadastroActivity.class);
+        startActivity(intent);
     }
-
-
-
-
-
-
-
 }
 
