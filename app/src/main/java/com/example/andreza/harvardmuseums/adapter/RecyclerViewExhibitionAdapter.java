599d1654295a84c55fa5ee17f1078c2c665fcd3b
Fabiana Wakate Ota <fabiana.ota@gmail.com>
2018-10-24 11:40:23 -0300

    exhibition test

--- app/src/main/java/com/example/andreza/harvardmuseums/adapter/RecyclerViewExhibitionAdapter.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/adapter/RecyclerViewExhibitionAdapter.java
@@ -1,4 +1,64 @@
 package com.example.andreza.harvardmuseums.adapter;
 
-public class RecyclerViewExhibitionAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.andreza.harvardmuseums.R;
+import com.example.andreza.harvardmuseums.model.Exhibition;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RecyclerViewExhibitionAdapter extends RecyclerView.Adapter<RecyclerViewExhibitionAdapter.ViewHolder> {
+
+    private List<Exhibition> exhibitionList;
+
+    public RecyclerViewExhibitionAdapter(List<Exhibition> exhibitionList) {
+        this.exhibitionList = exhibitionList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewExhibitionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.content_exhibition_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewExhibitionAdapter.ViewHolder viewHolder, int position) {
+        Exhibition exhibition = exhibitionList.get(position);
+        viewHolder.bind(exhibition);
+    }
+
+    @Override
+    public int getItemCount() {
+        return exhibitionList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private TextView date;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.textView_nameExhibition_id);
+            date = itemView.findViewById(R.id.textView_dateExhibition_id);
+
+        }
+
+        public void bind (Exhibition exhibition) {
+
+            name.setText(exhibition.getTitle());
+            date.setText(exhibition.getDate());
+
+        }
+    }
 }
--- app/src/main/java/com/example/andreza/harvardmuseums/model/Exhibition.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/model/Exhibition.java
@@ -1,4 +1,31 @@
 package com.example.andreza.harvardmuseums.model;
 
-public class Exhibition {
+import java.io.Serializable;
+
+public class Exhibition implements Serializable {
+
+    private String title;
+    private String date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Exhibition(String title, String date) {
+        this.title = title;
+        this.date = date;
+    }
+
 }
