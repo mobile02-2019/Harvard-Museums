bf6d48d5100d185e5402e23ba587c7853689d940
Joao Ricardi <JoaoRicardi@MacBook-Air-de-Joao.local>
2018-12-11 10:25:49 -0200

    sem testa google

--- app/src/main/java/com/example/andreza/harvardmuseums/fragment/ExhibitionDetailFragment.java
+++ app/src/main/java/com/example/andreza/harvardmuseums/fragment/ExhibitionDetailFragment.java
@@ -76,51 +76,7 @@ public class ExhibitionDetailFragment extends Fragment {
         return view;
     }
 
-   /* private void setupRecyclerView(View view) {
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_exhibitionDetail_id);
-        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        RecyclerViewExhibitionDetailAdapter adapter = new RecyclerViewExhibitionDetailAdapter(createGalleryList(), listener);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(horizontalLayoutManager);
-    }
-
-    private List<Exhibition> createGalleryList() {
-        List<Exhibition> galleryList = new ArrayList<>();
-
-        Exhibition exhibition1 = new Exhibition();
-        exhibition1.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition1);
-
-        Exhibition exhibition2 = new Exhibition();
-        exhibition2.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition2);
-
-        Exhibition exhibition3 = new Exhibition();
-        exhibition3.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition3);
-
-        Exhibition exhibition4 = new Exhibition();
-        exhibition4.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition4);
-
-        Exhibition exhibition5 = new Exhibition();
-        exhibition5.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition5);
-
-        Exhibition exhibition6 = new Exhibition();
-        exhibition6.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition6);
-
-        Exhibition exhibition7 = new Exhibition();
-        exhibition7.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition7);
-
-        Exhibition exhibition8 = new Exhibition();
-        exhibition8.setImagemDetalhes(R.drawable.image_exhibition);
-        galleryList.add(exhibition8);
-
-        return galleryList;
-    }*/
+ 
 
    private void settarExhibition(){
        titulo.setText(exhibition.getTitle());
